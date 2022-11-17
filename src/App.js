d5e43b6fe2e0275cbe38ea22bfc44ddc9a408b48
Joaquin <96495159+joaquinjoaco@users.noreply.github.com>
2022-11-17 20:46:55 -0300

    progress

--- src/App.js
+++ src/App.js
@@ -44,16 +44,14 @@ function App() {
     setTodo("");
   }
 
-
-
   // update
   const handleUpdate = (todo) => {
     setIsEdit(true);
     setTempUuid(todo.uuid);
     setTodo(todo.todo);
-
   }
 
+  // update part 2
   const handleSubmitChange = () => {
     if (todo !== '') {
       update(ref(db, `/${tempUuid}`), {
@@ -61,36 +59,20 @@ function App() {
         uuid: tempUuid,
       });
       setIsEdit(false);
+
       setTodo("");
-
     }
-
   }
 
   // delete
   const handleDelete = (todo) => {
-    remove(ref(db, `/${todo.uuid}`));
+    remove(ref(db, `/${tempUuid}`));
+    setIsEdit(false);
   };
 
 
   return (
     <div className="App">
-      {/* <div className="input-container">
-          <input type="text" value={todo} onChange={handleTodoChange} />
-
-          {isEdit ? (
-            <>
-              <button onClick={handleSubmitChange}>Submit Change</button>
-              <button onClick={() => {
-                setIsEdit(false);
-                setTodo("");
-              }}
-              >Cancel</button>
-            </>
-          ) : (
-            <button onClick={writeToDatabase}>Submit</button>
-          )}
-        </div> */}
 
       <nav>
         <img src="/images/logoEgoodQ.png" alt="eventyr logo" className="eventyr-logo" />
@@ -143,39 +125,121 @@ function App() {
             </div>
             <div className="staff-container">
               <label>Staff</label>
-              <select
-              // value={ }
-              // onChange={(e) => setAuthor(e.target.value)}
-              >
-                <option value="Console">Console</option>
-                <option value="Temp ban">Ryon</option>
-                <option value="Ban IP">Fanatio</option>
-                <option value="Permanente">Dilong</option>
-                <option value="Mute">Kisin</option>
-                <option value="Temp mute">Sathiel</option>
-                <option value="Temp mute">Skaddi</option>
-                <option value="Temp mute">Lacerannte</option>
-                <option value="Temp mute">Ocaoj</option>
-                <option value="Temp mute">Huumbug</option>
-              </select>
+              <div className="select-wrapper">
+                <select
+                // value={ }
+                // onChange={(e) => setAuthor(e.target.value)}
+                >
+                  <option value="Console">Console</option>
+                  <option value="Temp ban">Ryon</option>
+                  <option value="Ban IP">Fanatio</option>
+                  <option value="Permanente">Dilong</option>
+                  <option value="Mute">Kisin</option>
+                  <option value="Temp mute">Sathiel</option>
+                  <option value="Temp mute">Skaddi</option>
+                  <option value="Temp mute">Lacerannte</option>
+                  <option value="Temp mute">Ocaoj</option>
+                  <option value="Temp mute">Huumbug</option>
+                </select>
+              </div>
             </div>
-            <button className="new-submit-btn" onClick={() => { writeToDatabase(); setIsHidden(true); }}>Submit</button>
+            <button className="new-submit-btn" onClick={() => { writeToDatabase(); setIsHidden(true); }}>Añadir</button>
           </div>
         </div>
       )}
 
       <div className="cards-wrapper">
+
         {todos.map((todo) => (
+
+
+
           <div className="card" key={todo.uuid}>
-            <h1>{todo.todo}</h1>
-            <span>
-              <button onClick={() => handleUpdate(todo)}>update</button>
-              <button onClick={() => handleDelete(todo)}>delete</button>
-            </span>
+            <div className="card-top-container">
+              <h1>{todo.todo}</h1>
+              <button className="card-update-btn" onClick={() => handleUpdate(todo)}><box-icon type='solid' name='pencil' color="white"></box-icon></button>
+
+            </div>
+            <h2 className="card-sanction">%sanction%</h2>
+            <p className="card-reason">%reason%</p>
+            <div className="card-bottom-container">
+              <p className="card-date">%date%</p>
+              <p className="card-staff">%staff%</p>
+            </div>
+
           </div>
+
         ))}
       </div>
-    </div>
+      {/* edit */}
+
+      {isEdit ? (
+        <div className="new-write-popup">
+          <div className="top-container">
+            <p className="card-edit-p">Editar registro</p>
+            <button className="new-close-btn" onClick={() => handleDelete(todo)}>
+              <box-icon name='trash-alt' type='solid' color="white" ></box-icon>
+            </button>
+            <button className="new-close-btn" onClick={() => {
+              setIsEdit(false);
+              setTodo("");
+            }}
+            >
+              <box-icon className="pelao" size="md" name="x" color="white"></box-icon>
+            </button>
+          </div>
+          <div className="inputs-container">
+            <div className="nickname-container">
+              <label>Nickname</label>
+              <input type="text" value={todo} onChange={handleTodoChange} />
+            </div>
+            <div className="sanction-container">
+              <label>Sanción</label>
+              <select
+              // value={ }
+              // onChange={(e) => setAuthor(e.target.value)}
+              >
+                <option value="Aviso">Aviso</option>
+                <option value="Temp ban">Temp ban</option>
+                <option value="Ban IP">Ban IP</option>
+                <option value="Permanente">Permanente</option>
+                <option value="Mute">Mute</option>
+                <option value="Temp mute">Temp mute</option>
+              </select>
+            </div>
+            <div className="reason-container">
+              <label>Razón</label>
+              <textarea></textarea>
+            </div>
+            <div className="staff-container">
+              <label>Staff</label>
+              <div className="select-wrapper">
+                <select
+                // value={ }
+                // onChange={(e) => setAuthor(e.target.value)}
+                >
+                  <option value="Console">Console</option>
+                  <option value="Temp ban">Ryon</option>
+                  <option value="Ban IP">Fanatio</option>
+                  <option value="Permanente">Dilong</option>
+                  <option value="Mute">Kisin</option>
+                  <option value="Temp mute">Sathiel</option>
+                  <option value="Temp mute">Skaddi</option>
+                  <option value="Temp mute">Lacerannte</option>
+                  <option value="Temp mute">Ocaoj</option>
+                  <option value="Temp mute">Huumbug</option>
+                </select>
+              </div>
+            </div>
+            <button className="new-submit-btn" onClick={handleSubmitChange}>Editar</button>
+
+          </div>
+        </div>
+      ) : (
+        <>
+        </>
+      )}
+    </div >
   );
 }
 
